8d6ba3ec97fe93a4eb838f474180b3a0aeededab
MSINF16B <mic.spahr.16@lehre.mosbach.dhbw.de>
2019-04-03 01:25:33 +0200

    Fixed datalayout string.

--- llvm/lib/Target/Digital/DigitalTargetMachine.cpp
+++ llvm/lib/Target/Digital/DigitalTargetMachine.cpp
@@ -32,46 +32,13 @@ static Reloc::Model getEffectiveRelocModel(Optional<Reloc::Model> RM) {
   return *RM;
 }
 
-static StringRef computeDataLayout(const Triple &T) {
-  std::string Ret = "";
-
-// "E-m:m-p:16:16-i16:16-n16-S16"
-/*
-  Ret += "E";
-  Ret += "-m:m"; 
-  Ret += "-p:16:16"; 
-  Ret += "-i16:16";
-  Ret += "-n16";   
-  Ret += "-S16"; 
-*/
-    Ret += "E"; // Big endian
-
-  Ret += "-m:e"; // ELF name mangling
-  // first value is pointer size, and the second value is both ABI 
-  //   and preferred alignment.
-  Ret += "-p:16:16"; // 16-bit pointers, 16 bit aligned
-  // Alignments for 16 bit integers.
-  Ret += "-i16:16"; // 16 bit integers, 16 bit aligned
-  Ret += "-a:0:16";  // 16 bit alignment of objects of aggregate type
-  Ret += "-n16"; // 16 bit native integer width
-  Ret += "-S16"; // 16 bit natural stack alignment
-  return Ret;
-}
-
-// DataLayout --> Big-endian, 32-bit pointer/ABI/alignment
-// The stack is always 8 byte aligned
-// On function prologue, the stack is created by decrementing
-// its pointer. Once decremented, all references are done with positive
-// offset from the stack/frame pointer, using StackGrowsUp enables
-// an easier handling.
-// Using CodeModel::Large enables different CALL behavior.
 DigitalTargetMachine::DigitalTargetMachine( const Target &T, const Triple &TT,
                                        StringRef CPU, StringRef FeatureString,
                                        const TargetOptions &Options,
                                        Optional<Reloc::Model> RM,
                                        Optional<CodeModel::Model> CodeModel,
                                        CodeGenOpt::Level OptLevel, bool JIT)
-    : LLVMTargetMachine(T, computeDataLayout(TT), TT, CPU, FeatureString,
+    : LLVMTargetMachine(T, "E-m:m-p:16:16-i16:16-n16-S16", TT, CPU, FeatureString,
                         Options, getEffectiveRelocModel(RM),
                         getEffectiveCodeModel(CodeModel, CodeModel::Small), OptLevel),
       TLOF(make_unique<DigitalTargetObjectFile>()) {
